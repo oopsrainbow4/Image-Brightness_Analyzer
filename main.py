1b511e66560c1274e18e4e8782d688a5cc43ce2d
gr301847 <oopsrainbowytbig@gmail.com>
2026-06-16 16:29:15 +0200

    Create histogram amount of same brightness.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
-# "medkit_phighting.png" and "small_pixel_art_star.jpg"
-img = Image.open("ten_cents_coin.jpg")
+# "medkit_phighting.png", "small_pixel_art_star.jpg" and "ten_cents_coin.jpg"
+img = Image.open("medkit_phighting.png")
 convert_img = img.convert("L")
 
 img_array = np.array(convert_img)
@@ -10,6 +11,16 @@ img_array = np.array(convert_img)
 average_array = img_array.mean()
 maxium_value_array = np.max(img_array)
 minimal_value_array = np.min(img_array)
+
+img_1d_array = img_array.ravel()
+
+plt.hist(img_1d_array, bins=52, density=True)
+
+plt.xlabel("x-Axis: Brightness (0 - 255)")
+plt.ylabel("y-Axis: Amount of same brightness")
+plt.title("Image Brightness Analyzer")
+
+plt.show()
 
 print(f"Average brightness: {round(average_array, 2)}")
 print(f"Brightest  value: {maxium_value_array}")
